ae842a38143d161a0200ef631b16390ee645c797
chriswn <128608842+chriswn@users.noreply.github.com>
2026-05-26 17:22:01 -0500

    Refactor DrivetrainSubsystem to use SparkMax controllers

--- src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -4,10 +4,14 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-import edu.wpi.first.wpilibj.motorcontrol.MotorController;
-import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
@@ -15,37 +19,62 @@ public class DrivetrainSubsystem extends SubsystemBase {
 
   public double speed;
 
-  public MotorController motorControllerRightBack;
-  public MotorController motorControllerRightFront;
-  public MotorController motorControllerLeftFront;
-  public MotorController motorControllerLeftBack;
-
-  private MotorControllerGroup leftMotors;
-  private MotorControllerGroup rightMotors;
+  // Updated to modern SparkMax objects
+  public final SparkMax motorControllerLeftFront;
+  public final SparkMax motorControllerLeftBack;
+  public final SparkMax motorControllerRightFront;
+  public final SparkMax motorControllerRightBack;
 
   public DifferentialDrive tankDrive;
+
   /** Creates a new DrivetrainSubsystem. */
   public DrivetrainSubsystem() {
 
     speed = 1.00;
 
-    motorControllerLeftFront = new Spark(0);
-    motorControllerLeftBack = new Spark(1);
-    motorControllerRightFront = new Spark(2);
-    motorControllerRightBack = new Spark(3);
+    // Initialize SparkMax controllers using CAN IDs (0, 1, 2, 3) and set to
+    // Brushless for NEOs
+    motorControllerLeftFront = new SparkMax(1, MotorType.kBrushless);
+    motorControllerLeftBack = new SparkMax(2, MotorType.kBrushless);
+    motorControllerRightFront = new SparkMax(3, MotorType.kBrushless);
+    motorControllerRightBack = new SparkMax(4, MotorType.kBrushless);
 
-    this.leftMotors = new MotorControllerGroup(
-      this.motorControllerLeftFront,
-      this.motorControllerLeftBack
-    );
-    this.rightMotors = new MotorControllerGroup(
-      this.motorControllerRightFront,
-      this.motorControllerRightBack
-    );
-   
-    tankDrive = new DifferentialDrive(leftMotors, rightMotors);
+    // Create configuration objects
+    SparkMaxConfig globalConfig = new SparkMaxConfig();
+    SparkMaxConfig leftBackConfig = new SparkMaxConfig();
+    SparkMaxConfig rightFrontConfig = new SparkMaxConfig();
+    SparkMaxConfig rightBackConfig = new SparkMaxConfig();
 
-    rightMotors.setInverted(true);
+    // Set common settings (Brake mode and current limits protect your NEOs)
+    globalConfig
+        .smartCurrentLimit(50)
+        .idleMode(IdleMode.kBrake);
+
+    // Configure left follower to follow left leader
+    leftBackConfig
+        .apply(globalConfig)
+        .follow(motorControllerLeftFront);
+
+    // Configure right leader to be inverted (replaces rightMotors.setInverted)
+    rightFrontConfig
+        .apply(globalConfig)
+        .inverted(true);
+
+    // Configure right follower to follow right leader
+    rightBackConfig
+        .apply(globalConfig)
+        .follow(motorControllerRightFront);
+
+    // Apply the configurations to the physical controllers and save to flash memory
+    motorControllerLeftFront.configure(globalConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorControllerLeftBack.configure(leftBackConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+    motorControllerRightFront.configure(rightFrontConfig, ResetMode.kResetSafeParameters,
+        PersistMode.kPersistParameters);
+    motorControllerRightBack.configure(rightBackConfig, ResetMode.kResetSafeParameters, PersistMode.kPersistParameters);
+
+    // DifferentialDrive only needs the two leader motors now!
+    // The followers will mirror their leaders automatically.
+    tankDrive = new DifferentialDrive(motorControllerLeftFront, motorControllerRightFront);
   }
 
   @Override
@@ -58,10 +87,10 @@ public class DrivetrainSubsystem extends SubsystemBase {
     leftSpeed = leftSpeed * speed;
     rightSpeed = rightSpeed * speed;
 
-   SmartDashboard.putNumber("leftSpeed", leftSpeed);
-   SmartDashboard.putNumber("RightSpeed", rightSpeed);
+    SmartDashboard.putNumber("leftSpeed", leftSpeed);
+    SmartDashboard.putNumber("RightSpeed", rightSpeed);
 
-  tankDrive.tankDrive(leftSpeed, rightSpeed);
-
+    // DifferentialDrive handles sending the speed commands safely
+    tankDrive.tankDrive(leftSpeed, rightSpeed);
   }
 }
